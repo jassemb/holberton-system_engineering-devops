fc18c1b840395f8da2c502da4f8a403cb79c4ed9
jassemb <benalijassem201@gmail.com>
2022-01-31 22:50:49 +0100

    3-dictionary_of_list_of_dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
 """
- export data in the JSON format
+    Python script that, exports data in the JSON format.
 """
-import requests
+
 import json
+import requests
+import sys
 
-if __name__ == '__main__':
-    employee_id = 1
-    user_tasks = {}
-    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
-    url_user = 'https://jsonplaceholder.typicode.com/users/'
-    users = requests.get(url_user).json()
+if __name__ == "__main__":
+    usr_url = "https://jsonplaceholder.typicode.com/users/"
+    users = requests.get(usr_url).json()
 
-    for employee_id in range(1, len(users) + 1):
-        todo = requests.get(url_todo, params={'userId': employee_id})
-        user = requests.get(url_user, params={'id': employee_id})
+    data = {}
 
-        todo_dict_list = todo.json()
-        user_dict_list = user.json()
-        task_list = []
-        employee = user_dict_list[0].get('username')
+    for user in users:
+        id = user.get("id")
+        url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+        todos = requests.get(url).json()
 
-        for task in todo_dict_list:
-            status = task.get('completed')
-            title = task.get('title')
-            task_dict = {}
-            task_dict['task'] = title
-            task_dict['completed'] = status
-            task_dict['username'] = employee
-            task_list.append(task_dict)
-        user_tasks[employee_id] = task_list
+        tasks = []
+        for t in todos:
+            tasks.append({"task": t.get("title"),
+                          "completed": t.get("completed"),
+                          "username": user.get("username")})
+        data["{}".format(id)] = tasks
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump(user_tasks, jsonfile)
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump({int(x): data[x] for x in data.keys()}, f, sort_keys=True)
